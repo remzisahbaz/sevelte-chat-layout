205015847ba9b2eb6c5cdfe3ffc01493e29633b3
RShbz <remzisahbaz@outlook.com>
2022-04-22 00:04:25 +0300

    create svelte

---
+++ frontend/src/routes/__layout.svelte
@@ -0,0 +1,56 @@
+<!--main skeletal structure -->
+<!--layout-->
+
+<!--script-->
+<script>
+	import Header from '$lib/header/Header.svelte';
+	import '../app.css';
+</script>
+
+<!--header-->
+<Header  message=""/>
+
+<!--body-->
+<main>
+	<slot />
+</main>
+
+<!--footer-->
+<footer>
+
+</footer>
+
+<!--styles-->
+<style>
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		padding: 1rem;
+		width: 100%;
+		max-width: 1024px;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+
+	footer {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding: 40px;
+		color: #ff3e00;
+		font-size-adjust: revert;
+	}
+
+	footer a {
+		font-weight: bold;
+	}
+
+	@media (min-width: 480px) {
+		footer {
+			padding: 150px ;
+
+		}
+	}
+</style>
---
+++ frontend/src/routes/project.svelte
@@ -0,0 +1,59 @@
+<!--script-->
+<script context="module">
+export async function load({fetch})
+{
+	// eslint-disable-next-line no-unused-vars
+	const res= await fetch('https://jsonplaceholder.typicode.com/posts');
+	// eslint-disable-next-line no-unused-vars
+	const guides=await res.json();
+
+	if(res.ok){
+		return {
+			props:{
+				guides
+			}
+		}
+	}
+	return {
+		status:res.status,
+		error:new Error('Could not fetch the guides')
+	};
+}
+
+</script>
+
+<script>
+	export  let guides
+
+</script>
+<!-- web site head-->
+<svelte:head>
+	<title>Project</title>
+</svelte:head>
+
+<!--body-->
+<body class="content">
+	<h1>Project this app</h1>
+
+	<div>
+		<ul>
+			{#each guides as guide}
+				<li>
+					<a href="/">{guide.title}</a>
+				</li>
+
+				{/each}
+		</ul>
+	</div>
+</body>
+
+
+
+<!--style-->
+<style>
+	.content {
+		width: 100%;
+		max-width: var(--column-width);
+		margin: var(--column-margin-top) auto 0 auto;
+	}
+</style>
